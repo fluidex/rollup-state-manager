aad301e42f8ed86051cca6807d228ecff57d909e
HAOYUatHZ <haoyu@protonmail.com>
2021-03-24 18:30:02 +0800

    scaffolding project

---
+++ src/bin/main.rs
@@ -0,0 +1,15 @@
+use state_keeper::config;
+
+fn main() {
+    dotenv::dotenv().ok();
+    env_logger::init();
+    log::info!("state_keeper started");
+
+    let mut conf = config_rs::Config::new();
+    let config_file = dotenv::var("CONFIG").unwrap();
+    conf.merge(config_rs::File::with_name(&config_file)).unwrap();
+    let settings: config::Settings = conf.try_into().unwrap();
+    log::debug!("{:?}", settings);
+
+    unimplemented!();
+}
