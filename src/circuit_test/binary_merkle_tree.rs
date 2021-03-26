3d91a3c60c58eb47be72def4060d6b7b82a08056
Zhang Zhuo <mycinbrin@gmail.com>
2021-03-26 17:15:34 +0800

    feature: add merkle tree; add basic circuit test framework (#1)
    * feature: add merkle tree; add basic circuit test framework
    
    * clippy

---
+++ src/circuit_test/binary_merkle_tree.rs
@@ -0,0 +1,42 @@
+// from https://github1s.com/Fluidex/circuits/blob/HEAD/test/binary_merkle_tree.ts
+use super::types::*;
+use crate::state::merkle_tree::Tree;
+use serde_json::json;
+
+use franklin_crypto::bellman::bn256::Fr;
+use franklin_crypto::bellman::{Field, PrimeField};
+
+pub fn test_check_leaf_update() -> CircuitTestCase {
+    let leaves: Vec<Fr> = vec![10, 11, 12, 13]
+        .iter()
+        .map(|x| Fr::from_str(&format!("{}", x)).unwrap())
+        .collect();
+    let mut tree = Tree::new(2, Fr::zero());
+    tree.fill_with_leaves_vec(&leaves);
+    let proof1 = tree.get_proof(2);
+    tree.set_value(2, Fr::from_str("19").unwrap());
+    let proof2 = tree.get_proof(2);
+    // TODO: we need a path index function?
+    //
+    let field_slice_to_string = |arr: &[Fr]| arr.iter().map(field_to_string).collect::<Vec<String>>();
+    let input = json!({
+        "enabled": 1,
+        "oldLeaf": field_to_string(&proof1.leaf),
+        "oldRoot": field_to_string(&proof1.root),
+        "newLeaf": field_to_string(&proof2.leaf),
+        "newRoot": field_to_string(&proof2.root),
+        "path_elements": proof1.path_elements.iter().map(|x| field_slice_to_string(x)).collect::<Vec<_>>(),
+        "path_index": [0, 1],
+    });
+    CircuitTestCase {
+        source: CircuitSource {
+            src: "src/lib/binary_merkle_tree.circom".to_owned(),
+            main: "CheckLeafUpdate(2)".to_owned(),
+        },
+        data: CircuitTestData {
+            name: "test_check_leaf_update".to_owned(),
+            input,
+            output: json!({}),
+        },
+    }
+}
---
+++ src/circuit_test/types.rs
@@ -0,0 +1,33 @@
+//use serde_json::Value;
+use regex::Regex;
+
+use franklin_crypto::bellman::bn256::Fr;
+use franklin_crypto::bellman::to_hex;
+use num_bigint::BigInt;
+
+#[derive(Default, Clone)]
+pub struct CircuitTestData {
+    pub name: String,
+    pub input: serde_json::Value,
+    pub output: serde_json::Value,
+}
+#[derive(Default, Clone)]
+pub struct CircuitSource {
+    pub src: String,
+    pub main: String,
+}
+#[derive(Default, Clone)]
+pub struct CircuitTestCase {
+    pub source: CircuitSource,
+    pub data: CircuitTestData,
+}
+
+pub fn format_circuit_name(s: &str) -> String {
+    // js: s.replace(/[ )]/g, '').replace(/[(,]/g, '_');
+    let remove = Regex::new(r"[ )]").unwrap();
+    let replace = Regex::new(r"[(,]").unwrap();
+    replace.replace_all(&remove.replace_all(s, ""), "_").to_owned().to_string()
+}
+pub fn field_to_string(elem: &Fr) -> String {
+    BigInt::parse_bytes(to_hex(elem).as_bytes(), 16).unwrap().to_str_radix(10)
+}
