37daa277f5ea1243e15bf836520ee2e59da678ae
HAOYUatHZ <haoyu@protonmail.com>
2021-04-12 15:20:04 +0800

    minor

--- src/tests/bin/test_global_state.rs
+++ src/tests/bin/test_global_state.rs
@@ -15,6 +15,37 @@ use std::io::{BufRead, BufReader, Lines, Write};
 use std::path::{Path, PathBuf};
 use std::time::Instant;
 
+// TODO: move to other? or at least re-org in tests?
+pub mod test_params {
+    pub const NTXS: usize = 2;
+    pub const BALANCELEVELS: usize = 2;
+    pub const ORDERLEVELS: usize = 7;
+    pub const ACCOUNTLEVELS: usize = 2;
+    pub const MAXORDERNUM: usize = 2usize.pow(ORDERLEVELS as u32);
+    pub const MAXACCOUNTNUM: usize = 2usize.pow(ACCOUNTLEVELS as u32);
+    pub const MAXTOKENNUM: usize = 2usize.pow(BALANCELEVELS as u32);
+    pub const VERBOSE: bool = false;
+
+    // TODO: put in util
+    // TODO: enum, from?
+    pub fn token_id(token_name: &str) -> u32 {
+        match token_name {
+            "ETH" => 0,
+            "USDT" => 1,
+            _ => unreachable!(),
+        }
+    }
+
+    // TODO: put in util
+    // TODO: enum, impl?
+    pub fn prec(token_id: u32) -> u32 {
+        match token_id {
+            0 | 1 => 6,
+            _ => unreachable!(),
+        }
+    }
+}
+
 enum WrappedMessage {
     BALANCE(messages::BalanceMessage),
     TRADE(messages::TradeMessage),
