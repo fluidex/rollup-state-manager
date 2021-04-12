6b0dab854fbdd286cdac1e7709a5018df86df377
HAOYUatHZ <haoyu@protonmail.com>
2021-04-12 15:10:56 +0800

    move test_params

--- src/bin/global_state_test.rs
+++ src/bin/global_state_test.rs
@@ -63,32 +63,6 @@ impl AsMut<PlaceOrderType> for PlaceOrder {
     }
 }
 
-mod test_params {
-    pub const NTXS: usize = 2;
-    pub const BALANCELEVELS: usize = 2;
-    pub const ORDERLEVELS: usize = 7;
-    pub const ACCOUNTLEVELS: usize = 2;
-    pub const MAXORDERNUM: usize = 2usize.pow(ORDERLEVELS as u32);
-    pub const MAXACCOUNTNUM: usize = 2usize.pow(ACCOUNTLEVELS as u32);
-    pub const MAXTOKENNUM: usize = 2usize.pow(BALANCELEVELS as u32);
-    pub const VERBOSE: bool = false;
-
-    pub fn token_id(token_name: &str) -> u32 {
-        match token_name {
-            "ETH" => 0,
-            "USDT" => 1,
-            _ => unreachable!(),
-        }
-    }
-
-    pub fn prec(token_id: u32) -> u32 {
-        match token_id {
-            0 | 1 => 6,
-            _ => unreachable!(),
-        }
-    }
-}
-
 #[derive(Clone, Copy)]
 struct TokenIdPair(u32, u32);
 /*
--- src/lib.rs
+++ src/lib.rs
@@ -2,5 +2,8 @@
 #![allow(dead_code)]
 pub mod config;
 pub mod state;
+
 // TODO: use some cargo feature for this
 pub mod circuit_test;
+
+pub mod tests;
