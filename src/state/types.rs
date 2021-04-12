ff5d3845f8a2f48613714243f4962ff877a9967c
HAOYUatHZ <haoyu@protonmail.com>
2021-04-12 16:59:47 +0800

    more revert

--- src/state/types.rs
+++ src/state/types.rs
@@ -19,38 +19,30 @@ pub fn hash(inputs: &[Fr]) -> Fr {
 
 // if use poseidon
 pub type Fr = poseidon_rs::Fr;
-
 lazy_static! {
     //pub static ref POSEIDON_PARAMS: poseidon_rs::Constants = poseidon_rs::load_constants();
     pub static ref POSEIDON_HASHER: poseidon_rs::Poseidon = poseidon_rs::Poseidon::new();
 }
-
 pub fn hash(inputs: &[Fr]) -> Fr {
     (&POSEIDON_HASHER).hash(inputs.to_vec()).unwrap()
 }
-
 pub fn shl(a: &Fr, x: u32) -> Fr {
     let mut repr = a.into_repr();
     repr.shl(x);
     Fr::from_repr(repr).unwrap()
 }
-
 pub fn u32_to_fr(x: u32) -> Fr {
     Fr::from_str(&format!("{}", x)).unwrap()
 }
-
 pub fn u64_to_fr(x: u64) -> Fr {
     Fr::from_repr(poseidon_rs::FrRepr::from(x)).unwrap()
 }
-
 pub fn field_to_u32(x: &Fr) -> u32 {
     field_to_string(x).parse::<u32>().unwrap()
 }
-
 pub fn field_to_bigint(elem: &Fr) -> BigInt {
     BigInt::parse_bytes(to_hex(elem).as_bytes(), 16).unwrap()
 }
-
 pub fn field_to_string(elem: &Fr) -> String {
     field_to_bigint(&elem).to_str_radix(10)
 }
