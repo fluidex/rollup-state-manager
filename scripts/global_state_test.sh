9d74824ef7885a60c1446a1ce3fd6e0a4e735ff1
HAOYUatHZ <haoyu@protonmail.com>
2021-05-16 20:03:40 +0800

    git pull submodules

--- scripts/global_state_test.sh
+++ scripts/global_state_test.sh
@@ -5,12 +5,9 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 cd $DIR/..
 
 # make sure submodule is correctly cloned!!
-# cd circuits; npm i
+git submodule update --init --recursive
+# git pull --recurse-submodules
+cd circuits; npm i
 cargo run --release --bin test_global_state
 snarkit --version || npm -g install snarkit
 snarkit test circuits/testdata/Block_2_2_7_2/ --backend=wasm
-
-
-
-
-
