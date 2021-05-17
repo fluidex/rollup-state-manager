50f4264b93028ca3a2f4f2a6187635916e736099
HAOYUatHZ <haoyu@protonmail.com>
2021-05-18 07:32:30 +0800

    improve test scripts

--- tests/global_state/test.sh
+++ tests/global_state/test.sh
@@ -2,13 +2,14 @@
 set -uex
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-cd $DIR/../..
+REPO_DIR=$DIR/"../.."
+cd $REPO_DIR
 
 # make sure submodule is correctly cloned!!
 git submodule update --init --recursive
 git pull --recurse-submodules
 cargo run --release --bin gen_global_state_testcase
 
-cd $DIR/../../circuits; npm i
+cd $REPO_DIR/circuits; npm i
 snarkit --version || npm -g install snarkit
 snarkit test testdata/Block_2_2_7_2/ --force_recompile --backend=wasm
