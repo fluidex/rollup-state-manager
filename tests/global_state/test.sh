a3fdeb5c6883855aa91c5bb05f126abc60335df7
HAOYUatHZ <haoyu@protonmail.com>
2021-05-17 14:56:46 +0800

    fix test_global_state script

--- tests/global_state/test.sh
+++ tests/global_state/test.sh
@@ -2,13 +2,13 @@
 set -uex
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-cd $DIR/..
+cd $DIR/../..
 
 # make sure submodule is correctly cloned!!
 git submodule update --init --recursive
 git pull --recurse-submodules
 cargo run --release --bin gen_global_state_testcase
 
-cd $DIR/../circuits; npm i
+cd $DIR/../../circuits; npm i
 snarkit --version || npm -g install snarkit
 snarkit test testdata/Block_2_2_7_2/ --backend=wasm
