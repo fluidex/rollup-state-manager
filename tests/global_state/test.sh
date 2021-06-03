b20fe304614383d6c9db9cc86cc47a45f3f719a0
HAOYUatHZ <haoyu@protonmail.com>
2021-06-03 18:59:08 +0800

    fix test scripts

--- tests/global_state/test.sh
+++ tests/global_state/test.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 set -uex
 
+NTXS=2;
+BALANCELEVELS=2;
+ORDERLEVELS=3;
+ACCOUNTLEVELS=2;
+
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 REPO_DIR=$DIR/"../.."
 cd $REPO_DIR
@@ -12,4 +17,4 @@ cargo run --release --bin gen_global_state_testcase
 
 cd $REPO_DIR/circuits; npm i
 snarkit --version || npm -g install snarkit
-snarkit test testdata/Block_2_2_3_2/ --force_recompile --backend=wasm
+snarkit test testdata/Block_$NTXS_$BALANCELEVELS_$ORDERLEVELS_$ACCOUNTLEVELS/ --force_recompile --backend=wasm
