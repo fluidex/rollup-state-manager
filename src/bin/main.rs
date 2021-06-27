4f2ca1f34eaf86ce18697c138e6e3f169772c518
Steven Gu <asongala@163.com>
2021-06-27 02:56:52 +0000

    Saves l2 blocks to table `l2block` of rollup_state_manager DB.

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -8,7 +8,9 @@ use rollup_state_manager::params;
 use rollup_state_manager::state::{GlobalState, WitnessGenerator};
 use rollup_state_manager::test_utils::messages::WrappedMessage;
 use rollup_state_manager::types::l2::{L2Block, L2BlockSerde};
+use rollup_state_manager::types::primitives::fr_to_string;
 use sqlx::postgres::PgPool;
+use sqlx::Row;
 use std::time::{Duration, Instant, SystemTime, UNIX_EPOCH};
 
 #[tokio::main]
@@ -99,13 +101,55 @@ async fn run(settings: &config::Settings) {
     let loader_thread = msg_loader::load_msgs_from_mq(&settings.brokers, msg_sender);
     let replay_thread = replay_msgs(msg_receiver, blk_sender);
 
-    let db_pool = PgPool::connect(&settings.prover_cluster_db).await.unwrap();
+    let prover_cluster_db_pool = PgPool::connect(&settings.prover_cluster_db).await.unwrap();
+    let rollup_state_manager_db_pool = PgPool::connect(&settings.rollup_state_manager_db).await.unwrap();
     for block in blk_receiver.iter() {
-        save_block_to_db(&db_pool, block).await.unwrap();
+        if is_present_block(&rollup_state_manager_db_pool, &block).await.unwrap() {
+            continue;
+        }
+        save_block_to_rollup_state_manager_db(&rollup_state_manager_db_pool, &block)
+            .await
+            .unwrap();
+        save_task_to_prover_cluster_db(&prover_cluster_db_pool, block).await.unwrap();
     }
 
     loader_thread.map(|h| h.join().expect("loader thread failed"));
     replay_thread.map(|h| h.join().expect("loader thread failed"));
+}
+
+// Returns true if already present in DB, otherwise false.
+async fn is_present_block(pool: &PgPool, block: &L2Block) -> anyhow::Result<bool> {
+    match sqlx::query("select new_root from l2block where block_id = $1")
+        .bind(block.block_id as u32)
+        .fetch_one(pool)
+        .await
+    {
+        Ok(row) => {
+            let new_root: String = row.get(0);
+            if new_root == fr_to_string(&block.witness.new_root) {
+                log::debug!("skip same l2 block {} {}", block.block_id, new_root);
+            } else {
+                panic!("l2 block generation must be deterministic!");
+            }
+
+            Ok(true)
+        }
+        Err(sqlx::Error::RowNotFound) => Ok(false),
+        Err(error) => return Err(error.into()),
+    }
+}
+
+async fn save_block_to_rollup_state_manager_db(pool: &PgPool, block: &L2Block) -> anyhow::Result<()> {
+    let new_root = fr_to_string(&block.witness.new_root);
+    let witness = L2BlockSerde::from(block.witness.clone());
+    sqlx::query("insert into l2block (block_id, new_root, witness) values ($1, $2, $3)")
+        .bind(block.block_id as u32)
+        .bind(new_root)
+        .bind(sqlx::types::Json(witness))
+        .execute(pool)
+        .await?;
+
+    Ok(())
 }
 
 #[derive(sqlx::Type)]
@@ -124,7 +168,7 @@ enum TaskStatus {
     Proved,
 }
 
-async fn save_block_to_db(pool: &PgPool, block: L2Block) -> anyhow::Result<()> {
+async fn save_task_to_prover_cluster_db(pool: &PgPool, block: L2Block) -> anyhow::Result<()> {
     let input = L2BlockSerde::from(block.witness);
     let task_id = unique_task_id();
 
