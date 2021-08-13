014e6bf7c18a9614043dc4bae029638b0feee0c7
Zhang Zhuo <mycinbrin@gmail.com>
2021-08-13 15:49:29 +0800

    fix rollupapi envoy (#167)

--- examples/js/rest_client.ts
+++ examples/js/rest_client.ts
@@ -1,21 +1,23 @@
-import axios, {AxiosInstance} from "axios";
+import axios, { AxiosInstance } from "axios";
 
 class Client {
-    client: AxiosInstance;
+  client: AxiosInstance;
 
-    constructor(server = process.env.REST_ADDR || "http://127.0.0.1:8766/api") {
-        console.log("using REST", server);
-        this.client = axios.create({ baseURL: server, timeout: 1000 });
-    }
+  constructor(
+    server = process.env.REST_ADDR || "http://0.0.0.0:8765/rollupapi"
+  ) {
+    console.log("using REST", server);
+    this.client = axios.create({ baseURL: server, timeout: 1000 });
+  }
 
-    async l2BlocksQuery() {
-        let resp = await this.client.get(`/l2_blocks`);
-        if (resp.status === 200) {
-            return resp.data
-        } else {
-            throw new Error(`request failed with ${resp.status} ${resp.statusText}`)
-        }
+  async l2BlocksQuery() {
+    let resp = await this.client.get(`/l2_blocks`);
+    if (resp.status === 200) {
+      return resp.data;
+    } else {
+      throw new Error(`request failed with ${resp.status} ${resp.statusText}`);
     }
+  }
 }
 
 let restClient = new Client();
