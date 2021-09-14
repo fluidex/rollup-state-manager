fe0c2e0c588fc9278da8b96ab7c12925e9f28285
HAOYUatHZ <haoyu@protonmail.com>
2021-09-14 11:40:38 +0800

    format codes

--- examples/js/get_l2_block_by_id.ts
+++ examples/js/get_l2_block_by_id.ts
@@ -10,7 +10,7 @@ const kafkaUserValue = {
   user_id: userId,
   l1_address: "0x6286d0A2FC1d4C12a4ACc274018b401c68157Fdb",
   l2_pubkey:
-    "0x5d182c51bcfe99583d7075a7a0c10d96bef82b8a059c4bf8c5f6e7124cf2bba3"
+    "0x5d182c51bcfe99583d7075a7a0c10d96bef82b8a059c4bf8c5f6e7124cf2bba3",
 };
 
 const kafkaBalanceValue = {
@@ -22,7 +22,7 @@ const kafkaBalanceValue = {
   balance: "3",
   balance_available: "3",
   balance_frozen: "0",
-  detail: JSON.stringify({ id: 0 })
+  detail: JSON.stringify({ id: 0 }),
 };
 
 async function main() {
@@ -49,8 +49,8 @@ async function registerUser() {
   await kafkaProducer.send([
     {
       key: "registeruser",
-      value: JSON.stringify(kafkaUserValue)
-    }
+      value: JSON.stringify(kafkaUserValue),
+    },
   ]);
 }
 
@@ -58,8 +58,8 @@ async function depositBalance() {
   await kafkaProducer.send([
     {
       key: "deposits",
-      value: JSON.stringify(kafkaBalanceValue)
-    }
+      value: JSON.stringify(kafkaBalanceValue),
+    },
   ]);
 }
 
