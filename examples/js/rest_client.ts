ea7b4bfd14d5ba8a6e313bb591e9f7e8c6f6fd34
Steven Gu <asongala@163.com>
2021-11-10 14:07:15 +0000

    Refactor token balance query.

--- examples/js/rest_client.ts
+++ examples/js/rest_client.ts
@@ -22,6 +22,24 @@ class Client {
       throw new Error(`request failed with ${resp.status} ${resp.statusText}`);
     }
   }
+
+  async tokenBalanceQuery(accountId, tokenId, tokenAddress, tokenName) {
+    let params = null;
+    if (tokenId != null) {
+      params = `token_id=${tokenId}`;
+    } else if (tokenAddress != null) {
+      params = `token_address=${tokenAddress}`;
+    } else if (tokenName != null) {
+      params = `token_name=${tokenName}`;
+    }
+
+    let resp = await this.client.get(`/token_balance/${accountId}?${params}`);
+    if (resp.status === 200) {
+      return resp.data;
+    } else {
+      throw new Error(`request failed with ${resp.status} ${resp.statusText}`);
+    }
+  }
 }
 
 let restClient = new Client();
