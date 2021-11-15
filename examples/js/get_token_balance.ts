c3ba7fbc0ef074239b9a42feeb38554a51193e83
Steven Gu <asongala@163.com>
2021-11-15 03:49:41 +0000

    Fix precision of amount values in L2 block detail.

--- examples/js/get_token_balance.ts
+++ examples/js/get_token_balance.ts
@@ -114,7 +114,7 @@ async function getL2BlockTest() {
   const tx2 = res["decoded_txs"][1]["deposit_tx"];
   assert.equal(tx2["account_id"], userId);
   assert.equal(tx2["token_id"], 0);
-  assert.equal(tx2["amount"], "3");
+  assert.equal(tx2["amount"], "3.0000");
   assert.equal(tx2["old_balance"], "0.0000");
   assert.equal(tx2["new_balance"], "3.0000");
 
