0b606d1eba19195cb6a424c4e75ca7140555ccd9
Zhang Zhuo <mycinbrin@gmail.com>
2021-11-15 20:38:03 +0800

    upgrade to circom2

--- tests/circuit_tests/test.sh
+++ tests/circuit_tests/test.sh
@@ -16,8 +16,11 @@ git submodule update --init --recursive
 if [ -z ${CI+x} ]; then git pull --recurse-submodules; fi
 cargo run --bin gen_export_circuit_testcase # debug mode for fast compile
 
+export PATH=$PATH:~/bin
+which circom || bash scripts/install_deps.sh
+
 cd $REPO_DIR/circuits
 if [ -z ${CI+x} ]; then npm ci; fi
-snarkit --version || npm -g install snarkit
-snarkit test testdata/Block_$NTXS"_"$BALANCELEVELS"_"$ORDERLEVELS"_"$ACCOUNTLEVELS/ --force_recompile --backend=wasm
-snarkit test testdata/CheckLeafUpdate_2/ --force_recompile --backend=wasm
+snarkit2 --version || npm -g install snarkit2
+snarkit2 test testdata/Block_$NTXS"_"$BALANCELEVELS"_"$ORDERLEVELS"_"$ACCOUNTLEVELS/ --force_recompile --backend=wasm
+snarkit2 test testdata/CheckLeafUpdate_2/ --force_recompile --backend=wasm
